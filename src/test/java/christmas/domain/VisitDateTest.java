ddc30b22bba9ff5a0f271bc19d7c3038635c8b28
dh1010a <dh1010a@naver.com>
2023-11-11 14:28:05 +0900

    test: visitDate 유효성 검증 테스트 추가

--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -1,21 +1,32 @@
 package christmas.domain;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.exception.ExceptionMessage;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class VisitDateTest {
 
     String exceptionMessage = ExceptionMessage.INVALID_DATE_RANGE_ERROR.getMessage();
 
-    @Test
-    void 날짜범위_1일과_31일사이가_아니면_에러발생() {
-        //given
-        String input = "32";
+    @DisplayName("날짜범위가_1일과_31일사이가_아니면_에러발생")
+    @ValueSource(strings = {"0", "33", "40", "1000"})
+    @ParameterizedTest
+    void invalidDateRangeExceptionTest(String input) {
+        assertSimpleTest(() -> assertThatThrownBy(() -> new VisitDate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(exceptionMessage)
+        );
+    }
 
-        assertSimpleTest(() -> Assertions.assertThatThrownBy(() -> new VisitDate(input))
+    @DisplayName("정수형식의_날짜가_아니면_에러발생")
+    @ValueSource(strings = {"0 1", " ", "a", "first", "1-1"})
+    @ParameterizedTest
+    void invalidDateInputExceptionTest(String input) {
+        assertSimpleTest(() -> assertThatThrownBy(() -> new VisitDate(input))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(exceptionMessage)
         );
