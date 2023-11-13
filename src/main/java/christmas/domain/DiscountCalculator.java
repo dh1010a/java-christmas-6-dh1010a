ffb89cc693f1dce6d13239dbed4f0e70df1f1751
dh1010a <dh1010a@naver.com>
2023-11-14 03:25:57 +0900

    feat: 별표 쳐진 날짜에 할인해주는 기능 구현

--- src/main/java/christmas/domain/DiscountCalculator.java
+++ src/main/java/christmas/domain/DiscountCalculator.java
@@ -4,21 +4,24 @@ import christmas.domain.orders.Orders;
 import christmas.domain.visitDate.VisitDate;
 
 public class DiscountCalculator {
-    private static final int CHRISTMAS = 25;
+    private static final int CHRISTMAS = 25; //추후 리팩토링을 통해 Enum으로 분리
     private static final int CHRISTMAS_DISCOUNT_OFFSET = 1000;
     private static final int CHRISTMAS_DISCOUNT_INCREASE = 100;
     private static final int DAY_OF_THE_WEEK_DISCOUNT = 2023;
+    private static final int STAR_DAY_DISCOUNT = 1000;
 
     private final Orders orders;
     private final VisitDate visitDate;
+    private int totalOrderPrice;
 
     public DiscountCalculator(Orders orders, VisitDate visitDate) {
         this.orders = orders;
         this.visitDate = visitDate;
+        totalOrderPrice = orders.getTotalPriceBeforeDiscount();
     }
 
     private void getTotalDiscount() {
-
+        
     }
 
     private int calculateChristmasDiscount() {
@@ -36,6 +39,13 @@ public class DiscountCalculator {
             return orders.getMainMenuCount() * DAY_OF_THE_WEEK_DISCOUNT;
         }
         return orders.getDesertMenuCount() * DAY_OF_THE_WEEK_DISCOUNT;
+    }
+
+    private int calculateStarDayDiscount() {
+        if (visitDate.isStarDay()) {
+            return STAR_DAY_DISCOUNT;
+        }
+        return 0;
     }
 
     private boolean isBeforeChristmas(int date) {
