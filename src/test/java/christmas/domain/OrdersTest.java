3ae66d434b6412eba3ea6110c892516bbb938be1
dh1010a <dh1010a@naver.com>
2023-11-14 02:30:15 +0900

    test: 유효하지 않은 메뉴 형식 에러 테스트 추가

--- src/test/java/christmas/domain/OrdersTest.java
+++ src/test/java/christmas/domain/OrdersTest.java
@@ -1,11 +1,18 @@
 package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import christmas.domain.orders.Menu;
 import christmas.domain.orders.Orders;
 import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class OrdersTest {
 
@@ -54,4 +61,70 @@ public class OrdersTest {
         assertThat(orders.isEventTarget()).isEqualTo(false);
         assertThat(orders1.isEventTarget()).isEqualTo(true);
     }
+
+    @DisplayName("중복된_메뉴입력시_예외_반환")
+    @MethodSource("duplicatedMenuProvider")
+    @ParameterizedTest
+    void duplicatedMenuExceptionTest(List<String> input) {
+        assertThatThrownBy(() -> new Orders(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> duplicatedMenuProvider() {
+        return Stream.of(
+                arguments(List.of("티본스테이크-1", "티본스테이크-1")),
+                arguments(List.of("제로콜라-1,티본스테이크-1,제로콜라-1")),
+                arguments(List.of("아이스크림-1,제로콜라-1,아이스크림-1"))
+        );
+    }
+
+    @DisplayName("없는_메뉴입력시_예외_반환")
+    @MethodSource("nonexistentMenuProvider")
+    @ParameterizedTest
+    void nonexistentMenuExceptionTest(List<String> input) {
+        assertThatThrownBy(() -> new Orders(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> nonexistentMenuProvider() {
+        return Stream.of(
+                arguments(List.of("티본스테이크-1", "티본-1")),
+                arguments(List.of("제로콜라-1,티본스테이크-1,코카콜라-1")),
+                arguments(List.of("아이스크림-1,제로콜라-1,초코아이스크림-1"))
+        );
+    }
+
+    @DisplayName("메뉴주문갯수_예외_반환")
+    @MethodSource("invalidNumberMenuProvider")
+    @ParameterizedTest
+    void invalidNumberMenuExceptionTest(List<String> input) {
+        assertThatThrownBy(() -> new Orders(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> invalidNumberMenuProvider() {
+        return Stream.of(
+                arguments(List.of("티본스테이크-r", "타파스-1")),
+                arguments(List.of("제로콜라-1,티본스테이크-0,아이스크림-1")),
+                arguments(List.of("바비큐립-one,제로콜라-1,티본스테이크-1"))
+        );
+    }
+
+    @DisplayName("주문_형식이_틀릴시_예외_반환")
+    @MethodSource("invalidMenuFormatProvider")
+    @ParameterizedTest
+    void invalidMenuFormatExceptionTest(List<String> input) {
+        assertThatThrownBy(() -> new Orders(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Arguments> invalidMenuFormatProvider() {
+        return Stream.of(
+                arguments(List.of("티본스테이크1", "바비큐립-1")),
+                arguments(List.of("제로콜라-1,티본스테이크1개,바비큐립-1")),
+                arguments(List.of("아이스크림.1,제로콜라.1,바비큐립-1")),
+                arguments(List.of("아이스크림=1,제로콜라-1,바비큐립-1"))
+        );
+    }
+
 }
--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -72,4 +72,5 @@ public class VisitDateTest {
 
         assertThat(visitDate.isStarDay()).isEqualTo(false);
     }
+    
 }
